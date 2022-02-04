71b3d2ab7fdf530ab538cf68f22d166dbaeceea7
D.Jacklin <Dan@RusticMonkey.com>
2022-02-04 12:30:33 +0000

    NS-2: Added ship classes
    Classes for ships, guns and armour plus implementations for combatants at river plate

---
+++ src/gun.ts
@@ -0,0 +1,4 @@
+export class Gun {
+    constructor(private name: string, private range: number, private barrels: number, 
+        private rateOfFire: number, private calibre: number, private dualPurpose: boolean) {}
+}
